f35a29c78a826f56d7b2e0f583ed3c3379a2bfbb
Alejandro <alejandroluisacosta95@gmail.com>
2024-08-05 15:17:46 +0200

    add: decorative square on index page

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,11 +1,13 @@
 const swiperRooms = new Swiper('.swiper--rooms', {
     direction: 'horizontal',
     loop: true,
-  
+    
+    // slidesPerView: window.innerWidth > 1000 ?? 4,
     navigation: {
       nextEl: '#swiper-button-next--rooms',
       prevEl: '#swiper-button-prev--rooms',
     },
+    // spaceBetween: window.innerWidth > 1000 ?? 30,
   });
 
   if (window.innerWidth < 1000) {
