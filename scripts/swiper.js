3d7db83993bb34d0acdc970e1d65e51faa376351
Alejandro <alejandroluisacosta95@gmail.com>
2024-08-05 15:22:54 +0200

    add: carousel desktop styles

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -7,10 +7,10 @@ const swiperRooms = new Swiper('.swiper--rooms', {
       nextEl: '#swiper-button-next--rooms',
       prevEl: '#swiper-button-prev--rooms',
     },
-    // spaceBetween: window.innerWidth > 1000 ?? 30,
-  });
+  // spaceBetween: window.innerWidth > 1000 ?? 30,
+});
 
-  if (window.innerWidth < 1000) {
+if (window.innerWidth < 1000) {
     const swiperCoreFeatures = new Swiper('.swiper--facilities', {
       direction: 'horizontal',
       loop: true,
@@ -19,27 +19,29 @@ const swiperRooms = new Swiper('.swiper--rooms', {
         el: '.swiper-pagination',
       },
     });
-  }     
+}     
 
-  const swiperMenu = new Swiper('.swiper--menu', {
+const swiperMenu = new Swiper('.swiper--menu', {
     direction: 'horizontal',
     loop: true,
-  
+
     navigation: {
       nextEl: '#swiper-button-next--menu',
       prevEl: '#swiper-button-prev--menu',
     },
-  });
+});
 
-  const swiperAuto = new Swiper('.swiper--auto', {
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
+if (window.innerWidth < 1000) {
+    const swiperAuto = new Swiper('.swiper--auto', {
+      direction: 'horizontal',
+      loop: true,
+    
+      pagination: {
+        el: '.swiper-pagination',
+      },
 
-    autoplay: {
-      
-    },  
-  });
+      autoplay: {
+        
+      },  
+    });
+}
