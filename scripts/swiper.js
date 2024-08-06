9deb33fe90f30a2875f00186cd1b44f00ae74722
Alejandro <alejandroluisacosta95@gmail.com>
2024-08-06 16:24:57 +0200

    add: related rooms swiper at room details page 1st advancements

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -23,6 +23,20 @@ if (window.innerWidth < 1000) {
 }
 
 if (window.innerWidth < 1000) {
+  const swiperRelatedRooms = new Swiper('.swiper--related-rooms', {
+    direction: 'horizontal',
+    loop: true,
+    
+    // slidesPerView: window.innerWidth > 1000 ?? 4,
+    navigation: {
+      nextEl: '#swiper-button-next--rooms',
+      prevEl: '#swiper-button-prev--rooms',
+    },
+  // spaceBetween: window.innerWidth > 1000 ?? 30,
+  });
+}
+
+if (window.innerWidth < 1000) {
     const swiperCoreFeatures = new Swiper('.swiper--facilities', {
       direction: 'horizontal',
       loop: true,
